113ac18d9e524b86075c774f407ec9a96ad85df0
knoxgon <volkan.guven@hotmail.se>
2020-03-12 15:01:55 +0100

    Combined firebase and firestore reducers

--- src/__redux/reducer/root.js
+++ src/__redux/reducer/root.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import { userWithinPageLocationReducer } from "./userWithinPageLocationReducer";
+import { firebaseReducer } from "react-redux-firebase";
+import { firestoreReducer } from "redux-firestore";
 
 export const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
   userTrack: userWithinPageLocationReducer
 });
