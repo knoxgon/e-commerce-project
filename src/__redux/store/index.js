2436ec29564bb5f25b1d8415f14361873740e080
knoxgon <volkan.guven@hotmail.se>
2020-03-12 15:02:47 +0100

    Implemented the authentication ready state

--- src/__redux/store/index.js
+++ src/__redux/store/index.js
@@ -1,5 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import React from 'react';
+import { createStore, applyMiddleware, compose } from "redux";
 import { rootReducer } from "../reducer/root";
 import thunk from "redux-thunk";
+import { getFirebase, isLoaded } from 'react-redux-firebase';
+import { getFirestore, reduxFirestore } from 'redux-firestore';
+import firebase from '../../__config/firebase';
+import { useSelector } from "react-redux";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const AuthIsLoaded = ({ children }) => {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <div>splash screen...</div>;
+  return children
+}
+
+export const store = createStore(rootReducer, compose(applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})), reduxFirestore(firebase)))
