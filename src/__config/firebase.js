06b18cdcf3ef049fdc5a53508ea5e677999c4158
knoxgon <volkan.guven@hotmail.se>
2020-03-12 15:04:31 +0100

    Adapted the firebase conf

---
+++ src/__config/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+export default firebase.initializeApp({
+  apiKey: "AIzaSyBvYftpGzW48Uui4Kv2jbdcx3EQTQMOmjg",
+  authDomain: "ecomm-f69e7.firebaseapp.com",
+  databaseURL: "https://ecomm-f69e7.firebaseio.com",
+  projectId: "ecomm-f69e7",
+  storageBucket: "ecomm-f69e7.appspot.com",
+  messagingSenderId: "71683612197",
+  appId: "1:71683612197:web:9402ca4eb0d5f7345ba9cc",
+  measurementId: "G-74YEVFRR06"
+});
