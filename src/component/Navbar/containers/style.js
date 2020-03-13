cc654a642439b5564cf6ce0a2119123d2bc193bf
knoxgon <volkan.guven@hotmail.se>
2020-03-13 22:43:03 +0100

    Underneath shadow applied to hovered div

--- src/component/Navbar/containers/style.js
+++ src/component/Navbar/containers/style.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { NavbarLink } from '../style';
 import { Link } from 'react-router-dom';
 
 export const ImageSlideContainer = styled.div`
@@ -40,7 +39,13 @@ export const Container = styled.div`
   font-size: 1.65rem;
   background-color: #fdfdfd;
   padding: 1rem;
-  border-top: 2px solid #2DE262;
-  transition: all 0.30s linear;
+  border-top: 2px solid #2de262;
+  transition: all 0.3s linear;
   opacity: 0;
+  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+              0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+              0 12.5px 10px rgba(0, 0, 0, 0.06),
+              0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+              0 41.8px 33.4px rgba(96, 148, 88, 0.086),
+              0 100px 80px rgba(64, 255, 103, 0.12);
 `;
