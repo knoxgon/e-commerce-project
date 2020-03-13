ee1bc2ebadd23bf83239180267477321e710bc8b
knoxgon <volkan.guven@hotmail.se>
2020-03-13 02:00:45 +0100

    Removed unused library

--- src/__route/index.js
+++ src/__route/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './private';
 import Home from '../component/Home/home';
 import Owner from '../component/Owner/owner';
