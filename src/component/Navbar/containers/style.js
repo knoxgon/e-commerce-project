44a4fb96f551ba9c5363e3469148b8dc3f7864cb
knoxgon <volkan.guven@hotmail.se>
2020-03-13 22:36:37 +0100

    The sublink component on hovered div impl.

--- src/component/Navbar/containers/style.js
+++ src/component/Navbar/containers/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const ImageSlideContainer = styled.div`
   display: inline-flex;
@@ -10,4 +11,17 @@ export const InnerWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+`;
+
+export const SubLink = styled(Link)`
+  text-decoration: none;
+  text-decoration: inherit;
+  color: #1b9425;
+  width: fit-content;
+  margin: 0 auto;
+  font-weight: 500;
+  padding-bottom: 1rem;
+  &:hover {
+    color: green;
+  }
 `;
