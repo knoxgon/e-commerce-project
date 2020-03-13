f0ed4c669f1da97126e4a233b2ff67fff572469c
knoxgon <volkan.guven@hotmail.se>
2020-03-13 22:10:05 +0100

    Impl. parlor link list with route points

---
+++ src/component/Navbar/containers/sublinks.js
@@ -0,0 +1,11 @@
+const links = [
+  //Parlor
+  {
+    PARLOR: [
+      { name: "Sofa", route: "/parlor/sofa" },
+      { name: "Corner sofa", route: "/parlor/corner-sofa" },
+      { name: "TV bench", route: "/parlor/tv-bench" },
+      { name: "Coffee table", route: "/parlor/coffee-table" }
+    ]
+  },
+];
