a094eda3b45e063e4486e2efac4120787f1f600d
knoxgon <volkan.guven@hotmail.se>
2020-03-13 22:37:48 +0100

    The hover component design implemented

--- src/component/Navbar/containers/style.js
+++ src/component/Navbar/containers/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { NavbarLink } from '../style';
 import { Link } from 'react-router-dom';
 
 export const ImageSlideContainer = styled.div`
@@ -24,4 +25,22 @@ export const SubLink = styled(Link)`
   &:hover {
     color: green;
   }
+`;
+
+export const Container = styled.div`
+  position: absolute;
+  width: auto;
+  visibility: hidden;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  left: 10vw;
+  right: 25vw;
+  height: 20rem;
+  top: 7.6rem;
+  font-size: 1.65rem;
+  background-color: #fdfdfd;
+  padding: 1rem;
+  border-top: 2px solid #2DE262;
+  transition: all 0.35s linear;
+  opacity: 0;
 `;
