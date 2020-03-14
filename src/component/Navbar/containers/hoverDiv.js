8e9fca7addcc900c86de0569f5f3670355a37920
knoxgon <volkan.guven@hotmail.se>
2020-03-14 15:38:06 +0100

    Removed the redundant mouse event handlers

--- src/component/Navbar/containers/hoverDiv.js
+++ src/component/Navbar/containers/hoverDiv.js
@@ -1,23 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, SubLink, InnerWrapper, ImageSlideContainer } from './style';
 import Imager from '../../../__misc/slider/'
 import { dedicateLinkPage } from './sublinks';
 
 export const HoveredContainer = ({sentId}) => {
-  const [hover, setHover] = useState(false)
-
-  const mousehoveron = () => setHover(true)
-  const mousehoveroff = () => setHover(false)
-
   return (
-    <Container onMouseEnter={mousehoveron} onMouseLeave={mousehoveroff} >
+    <Container>
       <InnerWrapper>
         {dedicateLinkPage(sentId).map((item, i) => {
           return <SubLink to={item.route} key={i}>{item.name}</SubLink>
         })}
       </InnerWrapper>
       <ImageSlideContainer>
-        {hover && <Imager targetId={sentId} isHovering={hover}/>}
+        <Imager targetId={sentId}/>
       </ImageSlideContainer>
     </Container>
   )
