225772202a0255089f45a0bc511b25520d9c9844
knoxgon <volkan.guven@hotmail.se>
2020-03-15 01:20:28 +0100

    Set the rule for slide keyframe

--- src/component/GenericSlideShow/style.js
+++ src/component/GenericSlideShow/style.js
@@ -1,5 +1,10 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
+const slide = keyframes`
+  0% , 20%{ transform:   translateX(0%);}
+  40%, 70%{ transform:  translateX(-100%);}
+  90%, 100%{ transform:  translateX(-200%);}
+`
 export const SlideWrapper = styled.div`
   width: 100vw;
   overflow: hidden;
