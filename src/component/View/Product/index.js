835772ce786a16decfbbd7b7d5e63cbcc4bb358c
knoxgon <volkan.guven@hotmail.se>
2020-03-15 16:53:40 +0100

    Prepped Product component

---
+++ src/component/View/Product/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProductWrapper, ContentTitle, ProductFavorite, ProductFavoriteWrapper, ProductBoxTagBlur, ProductBoxTitle, ProductImageWrapper, ProductBox, ProductContainer, ProductBoxImage, ProductBoxBlur, ProductBoxTag, ProductPriceTag, ProductCart } from './style';
+
+const Product = (props) => {
+  return (
+    <ProductContainer>
+    </ProductContainer>
+  )
+}
+
