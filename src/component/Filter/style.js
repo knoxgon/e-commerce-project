ab79cb78453fbb9aa396e4e3002fe49610069109
knoxgon <volkan.guven@hotmail.se>
2020-03-16 12:04:54 +0100

    Set the tolerable filter dimension

--- src/component/Filter/style.js
+++ src/component/Filter/style.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 export const FilterAreaContainer = styled.div`
   display: flex;
   flex-direction: row;
+  width: calc(100vw - 57rem);
+  min-width: 40rem;
+  max-width: 123rem;
   margin: 0 auto;
 `;
 
